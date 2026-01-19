05e711091bdf9c5964d1b5cc4519b63e63ad5a13
lucasbaruj4 <barujalucas0@gmail.com>
2026-01-19 09:46:09 +0100

    embedding functions except embed_audio now return a dictionary with relevant metadata along with the embeddings

--- src/processing_indexing/embeddings.py
+++ src/processing_indexing/embeddings.py
@@ -9,22 +9,34 @@ from PIL import Image
 model = SentenceTransformer('clip-ViT-B-32')
 
 def embed_text(text_list):
-    text_emb = model.encode(text_list)
-    print(text_emb.shape)
-    return text_emb
+    # dictionary for returning the embeddings with the text
+    package = {}
+
+    # saving embeddings with text for retrieval
+    for n in range(len(text_list)):
+        text_emb = model.encode(text_list[n])
+        package[text_list[n]] = text_emb
+    print(package)
+    return package
 
 def embed_image(path):
+    # dictionary for returning the embeddings with the file path
+    package = {}
+    
     img_emb = model.encode(Image.open(path))
-    print(img_emb.shape)
-    return img_emb
+    package[path] = img_emb
+    print(package)
+    return package
 
 def embed_video(filenames):
-    opened_video_list = []
+    # same thing from above 
+    package = {}
+
     for n in range(len(filenames)):
-        opened_video_list.append(Image.open(filenames[n]))
-    
-    videos_emb = model.encode(opened_video_list)
-    print(videos_emb.shape)
-    return videos_emb
+        video_emb = model.encode(Image.open(filenames[n]))
+        package[filenames[n]] = video_emb
+
+    print(package)
+    return package
 
 # embed_audio implement!
